5359e69db625ad806f1ac36be2dc6e81f656c834
Stuart Lynn <stuart.lynn@gmail.com>
2022-04-14 14:20:38 -0400

    adding prompt structure

---
+++ pages/api/calls/_prompts.ts
@@ -0,0 +1,28 @@
+export const prompts =[
+  {
+    name:"Your Community",
+    categories: [
+
+      "How has your community been impacted by the pandemic? Some examples of your community may be your neighborhood, city, school, religious community, sports teams, or others.",
+      "What are some things you didn’t know about your community until the pandemic? Was anything revealed during this time?",
+      "How have you stayed connected to your community during the pandemic?"
+    ] 
+  },
+  {
+    name:"Your Family",
+    categories: [
+      "How did your family life change during the pandemic?",
+      "Did you or anyone close to you contract COVID- 19?",
+      "How did you navigate that experience?",
+      "Talk about any hardships or challenges your family faced during the pandemic.",
+    ] 
+  },
+  {
+    name:"Your Self",
+    categories: [
+      "Talk about a moment that you will remember most during this time.",
+      "Talk about the biggest challenge that you experienced during the pandemic.",
+      "What should people in the future take away or remember most, from your pandemic experience?",
+    ] 
+  }
+]
