f41b9031f53fc0f006d1cb979664ce2175e6442f
Stuart Lynn <stuart.lynn@gmail.com>
2022-04-14 12:23:44 -0400

    Incoming call hook

---
+++ pages/api/calls/incoming.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import twilio from "twilio";
+const VoiceResponse = twilio.twiml.VoiceResponse;
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<string>
+) {
+  if (req.method === 'POST') {
+
+    const twiml = new VoiceResponse();
+    twiml.say({ voice: 'alice' }, 'hello world!');
+
+    // Render the response as XML in reply to the webhook request
+    res.setHeader("content-type",'text/xml');
+    res.send(twiml.toString());
+  }
+}
+
