635c5c042e2caf9a56d2123866b505e7eef9f24a
theuscovidatlas <10041771+nofurtherinformation@users.noreply.github.com>
2022-07-05 13:03:35 -0500

    Update repair-av.ts

--- scripts/repair-av.ts
+++ scripts/repair-av.ts
@@ -18,7 +18,7 @@ interface FileObject {
 	Key: string | undefined
 	id: string | undefined
 	LastModified: Date | undefined
-	fileType: string
+	fileType: string | undefined
 }
 
 export interface PublicSubmission {
@@ -44,11 +44,13 @@ async function main() {
 	// from uploads (raw)
 	// and public folders
 	const uploadFileList = await getFileList(s3, S3_BUCKET, 'uploads/')
-	// clean to essential data needed
+	// running log of cleaned files, local to repo
 	let completedRepairs = readFileSync(
 		'./scripts/completed-repairs.txt',
 		'utf8'
 	).split(',')
+	// get a list of files, slim to essential
+	// filter for files needing repairs (mp4/mp3) and have not been repaired
 	const uploadContents: FileObject[] = uploadFileList?.Contents?.filter(
 		({ Key }) => !!Key && !Key.includes('_meta.json')
 	)
@@ -56,38 +58,55 @@ async function main() {
 			Key: Key || '',
 			LastModified,
 			id: Key && Key.split('/').slice(-1)[0].split('.')[0],
-			fileType: '.' + Key?.split('.').slice(-1)[0]
+			fileType: Key?.split('.').slice(-1)[0]
 		}))
 		.filter(
 			({ id, fileType }) =>
 				!completedRepairs.includes(id!) &&
-				(fileType === '.mp4' || fileType === '.mp3')
+				(fileType === 'mp4' || fileType === 'mp3')
 		)!
 
+	// if none, bueno
 	if (uploadContents.length === 0) {
 		console.log('No files to repair. Exiting.')
 	} else {
 		console.log(`Found ${uploadContents.length} files to repair.`)
+		// init ffmpeg and transcode function
+		// for mp4 files, this will also generate a gif
 		const ffmpeg = await initFfmpeg()
-		const doTranscode = async (filePath: string) => {
+		const doTranscode = async (filePath: string, isVideo: boolean) => {
 			ffmpeg.FS('writeFile', 'input.mp4', await fetchFile(filePath))
 			await ffmpeg.run('-i', 'input.mp4', '-c', 'copy', 'output.mp4')
-			return ffmpeg.FS('readFile', 'output.mp4')
+			isVideo && await ffmpeg.run('-i', 'input.mp4', '-vf','scale=320:-1, fps=1', 'output.gif')
+			return {
+				media: ffmpeg.FS('readFile', 'output.mp4'),
+				gif: isVideo && ffmpeg.FS('readFile', 'output.gif'),
+			}
 		}
+
+		// loop through, fetch file, transcode, upload
 		for (let i = 0; i < uploadContents.length; i++) {
 			const Key = uploadContents[i].Key!
 			const id = uploadContents[i].id!
+			const fileType = uploadContents[i].fileType!
+			const mimeType = fileType === 'mp4' ? 'video/mp4' : 'audio/mp3'
+			const isVideo = fileType === 'mp4'
 			try {
 				const response = await getPresignedUrl(
 					s3,
 					Key,
-					'video/mp4',
+					mimeType,
 					'',
 					'getObject'
 				)
 					.then((r) => r.url!)
-					.then((url) => doTranscode(url))
-					.then((data) => upload(s3, Key, 'video/mp4', data))
+					.then((url) => doTranscode(url, isVideo))
+					.then(({media, gif}) => {
+						const fileName = Key.split('.')[0]
+						isVideo && upload(s3, fileName + '.gif', 'image/gif', gif)
+						const mediaResponse = upload(s3, Key, mimeType, media)
+						return mediaResponse
+					})
 				if (response['$metadata'].httpStatusCode === 200) {
 					completedRepairs.push(id)
 					writeFileSync(
@@ -96,7 +115,7 @@ async function main() {
 					)
 				}
 			} catch (e) {
-				console.log('Repair failed', id)
+				console.log('Repair failed', id, e)
 			}
 		}
 	}
