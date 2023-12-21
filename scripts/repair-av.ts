20945732aa2609c9eaeda66a3af79ea22c9ef8a9
Adam Cox <mr.adamcox@gmail.com>
2023-12-21 15:12:21 -0600

    revert to copy vcodec :(

--- scripts/repair-av.ts
+++ scripts/repair-av.ts
@@ -91,7 +91,7 @@ async function main() {
 				'input.mp4',
 				// force H.264 encoding, better for streaming and compression
 				'-c:v',
-				'libx264',
+				'copy',
 				// using the aac audio encoder threw a "Pthread aborting at Error"
 				// error during testing, so just using copy
 				'-c:a',
